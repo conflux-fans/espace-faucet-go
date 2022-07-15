6dd76c57c5ea54d37b3237be0283558c566da296
Xianqi Yang <xqyang0202@163.com>
2022-07-15 15:15:42 +0800

    transfer to english

--- services/Captcha.go
+++ services/Captcha.go
@@ -9,17 +9,12 @@ import (
 var store = base64Captcha.DefaultMemStore
 
 func GetCaptcha(c *gin.Context) {
-	// height 高度 png 像素高度
-	// width  宽度 png 像素高度
-	// length 验证码默认位数
-	// maxSkew 单个数字的最大绝对倾斜因子
-	// dotCount 背景圆圈的数量
 	driver := base64Captcha.NewDriverDigit(80, 240, 5, 0.7, 80)
 	captcha := base64Captcha.NewCaptcha(driver, store)
 	id, content, err := captcha.Generate()
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
-			"msg": "生成验证码失败",
+			"msg": "failed to generate captcha",
 		})
 		return
 	}
@@ -32,9 +27,7 @@ func GetCaptcha(c *gin.Context) {
 func CheckCapcha(c *gin.Context) {
 	id := c.DefaultQuery("captchaId", "")
 	captchaVal := c.DefaultQuery("captchaVal", "")
-	// id 验证码id
-	// answer 需要校验的内容
-	// clear 校验完是否清除
+
 	res := store.Verify(id, captchaVal, true)
 	c.JSON(http.StatusOK, gin.H{
 		"data": res,
